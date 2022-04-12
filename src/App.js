b7a67d5f33603d5f457f91a523492565f0a94531
AzmiHabib <azmihabib0323@gmail.com>
2022-04-12 17:01:00 +0600

    set all routs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Products from './components/Products/Products';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signUp' element={<SignUp></SignUp>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Home from '../Home/Home';
+import Login from '../Login/Login';
+import Products from '../Products/Products';
 
 const Header = () => {
     return (
         <div>
-            <h2>this is header</h2>
+            <Link to='/' >Home</Link>
+            <Link to='/products'>Products</Link>
+            <Link to='/login'>LogIn</Link>
+            <Link to='/signUp'>SignUp</Link>
         </div>
     );
 };
