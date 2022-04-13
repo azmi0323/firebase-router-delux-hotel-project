f1b74e1642582a60d7e89b0081e3d0db3242601b
AzmiHabib <azmihabib0323@gmail.com>
2022-04-13 23:26:12 +0600

    ligin page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Login = () => {
     return (
         <div>
-            <h3>This is login</h3>
+            <div>
+      <Form className="container">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            
+            type="email"
+            placeholder="Enter email"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+        
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+       
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check  type="checkbox" label="Already have an account" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
         </div>
     );
 };
