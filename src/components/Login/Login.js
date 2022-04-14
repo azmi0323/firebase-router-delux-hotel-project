3292842d931e7f235b69774b9ef647d35bdf8e03
AzmiHabib <azmihabib0323@gmail.com>
2022-04-14 22:50:22 +0600

    login required

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
         <Form className="container text-primary w-50 mt-5 border p-4 rounded">
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" placeholder="Enter email" required/>
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
