c55aca65e0957b82879267d5c88169a4e963e835
AzmiHabib <azmihabib0323@gmail.com>
2022-04-14 22:51:21 +0600

    password requird

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" required/>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
